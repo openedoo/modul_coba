29bc133f5ab55607c51be44a8ef94b978789a76d
rendiya <ligerrendy@gmail.com>
2016-12-21 12:36:46 +0700

    update static file html etc

--- __init__.py
+++ __init__.py
@@ -1,6 +1,10 @@
-from openedoo.core.libs import blueprint
-
-module_coba = blueprint('modul_coba', __name__)
+#from openedoo.core.libs import blueprint,render_template
+from flask import render_template
+from flask import Blueprint as blueprint
+module_coba = blueprint('modul_coba', __name__,template_folder='static')
 @module_coba.route('/', methods=['POST', 'GET'])
 def index():
 	return "Hello World"
+@module_coba.route('/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
