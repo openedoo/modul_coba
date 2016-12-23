16befed30f95d5b4a0521f2cd1e396e3c493feca
rendiya <ligerrendy@gmail.com>
2016-12-23 16:07:44 +0700

    static file

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 #from openedoo.core.libs import blueprint,render_template
 from flask import render_template
 from flask import Blueprint as blueprint
-module_coba = blueprint('modul_coba', __name__,template_folder='static')
+module_coba = blueprint('modul_coba', __name__,static_url_path='/module_coba/static', static_folder='static',
+                          template_folder='static')
 @module_coba.route('/', methods=['POST', 'GET'])
 def index():
 	return "Hello World"
 @module_coba.route('/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    return render_template('hello2.html',name=name)
